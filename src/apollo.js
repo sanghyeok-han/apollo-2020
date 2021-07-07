2556b2ed96b5dca91e1078c4a210562d5c994a21
한상혁 <hsh8086@gmail.com>
2021-07-07 16:42:31 +0900

    ver 1

---
+++ src/apollo.js
@@ -0,0 +1,10 @@
+import ApolloClient from "apollo-boost"
+
+const client = new ApolloClient({
+  uri: "https://movie-ql.vercel.app/",
+})
+
+export default client
+// https://movieql.now.sh/
+// https://movieql-osezlvyqsg.now.sh/
+// https://movie-ql.vercel.app/
